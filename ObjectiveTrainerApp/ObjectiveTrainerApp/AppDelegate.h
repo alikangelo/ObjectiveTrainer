9e928081ceb29f541b6f3f25e44aafb5de13231e
alikangelo <alikangelo@yandex.ru>
2016-08-08 21:31:31 +0300

    Initial Project Commit
    Initial Project Commit

---
+++ ObjectiveTrainerApp/ObjectiveTrainerApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveTrainerApp
+//
+//  Created by Alik Hamdamov on 08.08.16.
+//  Copyright © 2016 AlisherKh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveTrainerApp/ObjectiveTrainerApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveTrainerApp
+//
+//  Created by Alik Hamdamov on 08.08.16.
+//  Copyright © 2016 AlisherKh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
